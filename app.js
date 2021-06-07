d8f666102ba277e37e7825b94f72b1d40deb6517
Mohammad Saad Ali <52074642+Mohammad-Saad-Aly@users.noreply.github.com>
2021-06-07 20:35:47 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,70 @@
+// var fname = "Saad"
+// var fname = "Ali"
+// document.write(fname)
+
+// let fname = "Saad"
+// fname = "Ali"
+// document.write(fname)
+
+// const fname = "saad"
+// fname = "Ali"
+// document.write(fname)
+
+// var a = 15
+// var b;
+// b = "Value is "+(a > 10 && a > 80 ? "true" : " false ")
+// document.write(b)
+// var num = prompt("enter num")
+// for(var a = 0; a<=100; a++){
+// if(num % 2 == 0){
+//     alert(num+"is even")
+// }
+// }
+
+// var arr = ["yasir","laiba","sabiqa","alex bhatti"]
+// // document.write(arr)
+// for(var a=0; a<arr.length; a++){
+//     document.write(arr[a]," ")
+// }
+
+
+// function hello(){
+//     alert("sohaib")
+// }
+
+// var arr = ["saad", 32 , "employee" , "ali" ,43,"student"]
+// document.write(arr)
+
+// var arr = [
+//     ["saad", 32, "male"],
+//     ["yasir", 54, "male"],
+//     ["laiba", 67, "female"],
+//     ["ulfat", 67, "female"],
+
+// ]
+// document.write("<table border='4'>")
+// for (var a = 0; a < arr.length; a++) {
+//     document.write("<tr>")
+//     for (var b = 0; b < arr[a].length; b++) {
+//         document.write("<td>"+arr[a][b], " "+"</td>")
+//     }
+//     // document.write("<br>")
+//     document.write("</tr>")
+// }
+// document.write("</table>")
+
+// var arr = ["uysama",'yasir','atif']
+// // document.write(arr,"<br>")
+// // arr[0] = "amir"
+// // document.write(arr)
+
+// delete arr[0]
+// document.write(arr)
+
+// var arr = ["saad","mohammad","ali"]
+// // var ok = ["Yasir","atif","ayesha"]
+// var ok =arr.join(" / ")
+// document.write(ok)
+// var ok = arr.concat("yasir","atif")
+// var c = arr.concat(ok)
+// document.write(c)
